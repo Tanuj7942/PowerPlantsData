95a64e54b91c11e4f3ee0362ba3f9ba7be2813db
Tanuj Agarwal <tagarwal@EX2INDIA.LOCAL>
2023-03-22 17:51:04 +0530

    removed the nested if-else

--- public/index2.js
+++ public/index2.js
@@ -15,14 +15,6 @@ var map = L.map('map', {
 });
 var myURL = jQuery('script[src$="index2.js"]').attr('src').replace('index2.js', '');
 
-var myIcon = L.icon({
-  iconUrl: myURL + 'mapbox-icon.png',
-  // iconRetinaUrl: myURL + 'images/pin48.png',
-  iconSize: [29, 24],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-});
-
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: ['a', 'b', 'c']
@@ -48,52 +40,58 @@ function addMarkers(markers) {
     var m;
     var popup = marker.name +
       '<br/>' + marker.country_name +
-      '<br/><b>Capacity</b> ' + marker.capacity +
-      '<br/><b>Primary Fuel</b> ' + marker.primaryfuel;
+      '<br/><b>Capacity: -</b> ' + marker.capacity +
+      '<br/><b>Primary Fuel: -</b> ' + marker.primaryfuel;
       // m = bindMarker(marker, popup, 'mapbox-icon.png')
-    if (marker.primaryfuel == "Hydro") {
-      m = bindMarker(marker, popup, '../Icons/icons8-hydro-60.png');
-    } else if (marker.primaryfuel == "Solar") {
-      m = bindMarker(marker, popup, '../Icons/icons8-solar-60.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-solar-60.png')"
-    } else if (marker.primaryfuel == "Gas") {
-      m = bindMarker(marker, popup, '../Icons/icons8-gas-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-gas-64.png')"
-    } else if (marker.primaryfuel == "Oil") {
-      m = bindMarker(marker, popup, '../Icons/icons8-oil-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-oil-64.png')"
-    } else if (marker.primaryfuel == "Wind") {
-      m = bindMarker(marker, popup, '../Icons/icons8-wind-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-wind-64.png')"
-    } else if (marker.primaryfuel == "Nuclear") {
-      m = bindMarker(marker, popup, '../Icons/icons8-nuclear-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-nuclear-64.png')"
-    } else if (marker.primaryfuel == "Coal") {
-      m = bindMarker(marker, popup, '../Icons/icons8-coal-100.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-coal-100.png')"
-    } else if (marker.primaryfuel == "Waste") {
-      m = bindMarker(marker, popup, '../Icons/icons8-wastes-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-wastes-64.png')"
-    } else if (marker.primaryfuel == "Biomass") {
-      m = bindMarker(marker, popup, '../Icons/icons8-biomass-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-biomass-64.png')"
-    } else if (marker.primaryfuel == "Wave and Tidal") {
-      m = bindMarker(marker, popup, '../Icons/icons8-sea-waves-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-sea-waves-64.png')"
-    } else if (marker.primaryfuel == "Petcoke") {
-      m = bindMarker(marker, popup, '../Icons/pngwing.com.png');
-      // el.style.backgroundImage = "url('../Icons/pngwing.com.png')"
-    } else if (marker.primaryfuel == "Geothermal") {
-      m = bindMarker(marker, popup, '../Icons/icons8-geothermal-energy-64.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-geothermal-energy-64.png')"
-    } else if (marker.primaryfuel == "Storage") {
-      m = bindMarker(marker, popup, '../Icons/icons8-storage-50.png');
-      // el.style.backgroundImage = "url('../Icons/icons8-storage-50.png')"
-    } else if (marker.primaryfuel == "Cogeneration") {
-      m = bindMarker(marker, popup, '../Icons/power-plant.png');
-      // el.style.backgroundImage = "url('../Icons/power-plant.png')"
-    } else {
+    // if (marker.primaryfuel == "Hydro") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-hydro-60.png');
+    // } else if (marker.primaryfuel == "Solar") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-solar-60.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-solar-60.png')"
+    // } else if (marker.primaryfuel == "Gas") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-gas-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-gas-64.png')"
+    // } else if (marker.primaryfuel == "Oil") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-oil-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-oil-64.png')"
+    // } else if (marker.primaryfuel == "Wind") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-wind-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-wind-64.png')"
+    // } else if (marker.primaryfuel == "Nuclear") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-nuclear-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-nuclear-64.png')"
+    // } else if (marker.primaryfuel == "Coal") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-coal-100.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-coal-100.png')"
+    // } else if (marker.primaryfuel == "Waste") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-wastes-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-wastes-64.png')"
+    // } else if (marker.primaryfuel == "Biomass") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-biomass-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-biomass-64.png')"
+    // } else if (marker.primaryfuel == "Wave and Tidal") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-sea-waves-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-sea-waves-64.png')"
+    // } else if (marker.primaryfuel == "Petcoke") {
+    //   m = bindMarker(marker, popup, '../Icons/pngwing.com.png');
+    //   // el.style.backgroundImage = "url('../Icons/pngwing.com.png')"
+    // } else if (marker.primaryfuel == "Geothermal") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-geothermal-energy-64.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-geothermal-energy-64.png')"
+    // } else if (marker.primaryfuel == "Storage") {
+    //   m = bindMarker(marker, popup, '../Icons/icons8-storage-50.png');
+    //   // el.style.backgroundImage = "url('../Icons/icons8-storage-50.png')"
+    // } else if (marker.primaryfuel == "Cogeneration") {
+    //   m = bindMarker(marker, popup, '../Icons/power-plant.png');
+    //   // el.style.backgroundImage = "url('../Icons/power-plant.png')"
+    // } else {
+    //   m = bindMarker(marker, popup, 'mapbox-icon.png');
+    // }
+
+    if(marker.primaryfuel == "Other") {
       m = bindMarker(marker, popup, 'mapbox-icon.png');
+    } else {
+      m = bindMarker(marker, popup, `../Icons/${marker.primaryfuel}.png`);
     }
 
     markerClusters.addLayer(m);
@@ -106,8 +104,9 @@ function bindMarker(marker, popup, path) {
   var m = L.marker([marker.geometry[1], marker.geometry[0]], {
     icon: L.icon({
       iconUrl: myURL + path,
+      shadowUrl: "",
       // iconRetinaUrl: myURL + 'images/pin48.png',
-      iconSize: [29, 24],
+      iconSize: [35, 35],
       iconAnchor: [9, 21],
       popupAnchor: [0, -14]
     })
